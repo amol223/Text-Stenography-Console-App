4acaf09cf9af3a700d3d0446343e16a13b4bdcbd
amol223 <amol223@gmail.com>
2017-10-14 14:31:38 -0400

    testing

--- TextConsoleApp2/Form1.cs
+++ TextConsoleApp2/Form1.cs
@@ -135,3 +135,4 @@ namespace TextConsole
         
     }
 }
+//The  advantage of  steganography  over  cryptography alone  is that  the intended secret  message does  not  attract attention  to itself as  an  object of scrutiny. Plainly  visible encrypted messages, no  matter  how unbreakable  they  are, arouse interest  and may  in  themselves  be incriminating in countries in which encryption is illegal. 
